e0b76a7e9e98ea7257a09c4ccb75343a5d141b9d
Kristin Evans <kristinlashun@gmail.com>
2023-10-04 11:26:29 -0700

    Added animal.py # Please enter the commit message for your changes. Lines starting

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Kristin Towns
+# GitHub username: kristinlashun
+# Date: 10/04/2023
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
